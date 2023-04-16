dd4b57087fa2ac006926c733c4b1a689eb1effbb
niharn22 <niharnandoskar@gmail.com>
2023-04-16 14:31:12 +0530

    second commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { useState } from 'react'
 import './App.css'
+import Task from './Task'
 
 function App() {
 
@@ -11,20 +12,43 @@ function App() {
   }
 
   const addTask = () => {
-    const newTodoList = [...todoList,newTask]
-    setTodoList(newTodoList)
+    const task = {
+      id:todoList.length === 0 ? 1 : todoList[todoList.length - 1].id+1,
+      taskName : newTask,
+      completed : false,
+    }
+    setTodoList([...todoList,task])
   }
+
+
+  const deleteTask = (id) => {
+    setTodoList(todoList.filter((task) => task.id !== id))
+  }
+
+  const completeTask = (id) => {
+    setTodoList(
+      todoList.map((task) => {
+        if (task.id === id) {
+          return { ...task, completed: true };
+        } else {
+          return task;
+        }
+      })
+    );
+  };
 
 
   return (
     <div className="App">
         <div className="addTask">
-          <input placeholder='Enter a task ' onChange={handleChange}/>
+          <input placeholder='Enter a task' onChange={handleChange}/>
           <button onClick={addTask}>Add Task</button>
         </div>
         <div className="List">
           {todoList.map((task) => {
-            return <h1>{task}</h1>
+            return (            
+              <Task taskName={task.taskName} id={task.id} completed={task.completed} deleteTask={deleteTask} completeTask={completeTask}/>
+            )
           })}
         </div>
     </div>
